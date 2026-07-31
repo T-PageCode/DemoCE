7f28da29bfb4d6244c5086b887ae7c011ef5ce30
T-PageCode <cdt20260@outlook.com>
2026-07-31 20:47:51 +0800

    主要更新字体和配置JSON等等

---
+++ app-page/js-show1.js
@@ -0,0 +1,8 @@
+setTimeout(() => {
+    div1.style.visibility = "visible";
+    div1.style.opacity = "1";
+    text2.style.visibility = "visible";
+    text2.style.opacity = "1";
+    btn1.style.visibility = "visible";
+    btn1.style.opacity = "1";
+},2500)
--- js-show1.js
+++
@@ -1,8 +0,0 @@
-setTimeout(() => {
-    div1.style.visibility = "visible";
-    div1.style.opacity = "1";
-    text2.style.visibility = "visible";
-    text2.style.opacity = "1";
-    btn1.style.visibility = "visible";
-    btn1.style.opacity = "1";
-},2500)
